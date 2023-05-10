ac33c8a2bf7ab6fb0c6a34d1026cddc34e3e000f
OneTooMany <santosismael.115@gmail.com>
2023-05-10 13:02:23 -0700

    Add background image to content class

--- src/homePage.js
+++ src/homePage.js
@@ -4,6 +4,9 @@ const home = function () {
   const div = document.createElement("div");
   div.classList.add("home-main-content");
   content.appendChild(div);
+
+  // Add background image to content class
+  content.style.backgroundImage = "url(../img/pinar-kucuk-unsplash.jpg)";
 };
 
 export default home;

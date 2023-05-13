0f915a778448ee959390f93598a5029a22701b11
OneTooMany <santosismael.115@gmail.com>
2023-05-13 16:24:32 -0700

    Remove unecessary code

--- src/loadPage.js
+++ src/loadPage.js
@@ -1,11 +1,9 @@
 import renderHeader from "./headerPage";
 import home from "./homePage";
-import menu from "./menuPage";
 
 const loadPage = function () {
   renderHeader();
-  /* home(); */
-  menu();
+  home();
 };
 
 export default loadPage;

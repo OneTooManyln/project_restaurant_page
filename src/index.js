3bde6f6d8a5b9f8256384df220ce0882857154d0
OneTooMany <santosismael.115@gmail.com>
2023-05-13 16:47:30 -0700

    Create tab switching logic with eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import loadPage from "./loadPage";
+import home from "./homePage";
+import menu from "./menuPage";
+import about from "./aboutPage";
+import empty from "./emptyPage";
 
 loadPage();
 
-console.log("this works");
+const homeTab = document.querySelector("#home");
+const menuTab = document.querySelector("#menu");
+const aboutTab = document.querySelector("#about");
+
+homeTab.addEventListener("click", function () {
+  empty();
+  home();
+});
+
+menuTab.addEventListener("click", function () {
+  empty();
+  menu();
+});
+
+aboutTab.addEventListener("click", function () {
+  empty();
+  about();
+});
+console.log(aboutTab);

3d98f903b41284bfcfd4b5e1c44444e450bd65cd
OneTooMany <santosismael.115@gmail.com>
2023-05-13 15:09:17 -0700

    Add unique id's to list elements

--- src/headerPage.js
+++ src/headerPage.js
@@ -11,16 +11,19 @@ const renderHeader = function () {
 
   // Add home page link
   const homeLink = document.createElement("li");
+  homeLink.setAttribute("id", "home");
   homeLink.innerText = "HOME";
   pageLinks.appendChild(homeLink);
 
   // Add menu page link
   const menuLink = document.createElement("li");
+  menuLink.setAttribute("id", "menu");
   menuLink.innerText = "MENU";
   pageLinks.appendChild(menuLink);
 
   // Add menu page link
   const aboutLink = document.createElement("li");
+  aboutLink.setAttribute("id", "about");
   aboutLink.innerText = "ABOUT";
   pageLinks.appendChild(aboutLink);
 };
